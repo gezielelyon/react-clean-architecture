c0ac4dbb8a0e57aeccf53bc5cebc48160c558428
Geziel Elyon <geziel_barbosa_@hotmail.com>
2022-03-31 14:00:56 -0300

    feat: ensure RemoteAuthentication returns an Account on success

--- src/data/usecases/authentication/remoteAuthentication.ts
+++ src/data/usecases/authentication/remoteAuthentication.ts
@@ -1,24 +1,24 @@
 import { IHttpPostClient } from '@/data/protocols/http/httpPostClient'
 import { HttpStatusCode } from '@/data/protocols/http/httpResponse'
 
+import { AuthenticationParams, IAuthentication } from '@/domain/usecases/authentication'
 import { InvalidCredentialsError } from '@/domain/errors/invalidCredentialsError'
 import { UnexpectedError } from '@/domain/errors/unexpectedError'
 import { AccountModel } from '@/domain/models/accountModel'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements IAuthentication {
   // All that's not common to every class instancy is passed by constructor
   constructor (
     private readonly url: string,
     private readonly httpPostClient: IHttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  public async auth (params: AuthenticationParams): Promise<void> {
+  public async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
 
     switch (httpResponse.status) {
       case HttpStatusCode.ok:
-        break
+        return httpResponse.body
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
       default:
--- src/domain/tests/mockAuthentication.ts
+++
@@ -1,8 +0,0 @@
-import faker from 'faker'
-
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-
-export const mockAuthentication = (): AuthenticationParams => ({
-  email: faker.internet.email(),
-  password: faker.internet.password()
-})
